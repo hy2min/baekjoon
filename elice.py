76bfc0cba483f15ea2a94e6cd7c3c9c4c4b3aef9
hyemin lee <hy2min@gmail.com>
2024-07-09 17:25:32 +0900

    solve : 숫자 판별

--- elice.py
+++ elice.py
@@ -1,16 +1,30 @@
 
-s = list(str(n))
-leng = len(s)
+# s = list(str(n))
+# leng = len(s)
 
-i = leng - 2
-while i >= 0 and s[i] >= s[i+1] :
-    i -= 1
+# i = leng - 2
+# while i >= 0 and s[i] >= s[i+1] :
+#     i -= 1
 
-j = leng - 1
-while s[j] <= s[i] :
-    j -= 1
-s[i], s[j] = s[j], s[i]
+# j = leng - 1
+# while s[j] <= s[i] :
+#     j -= 1
+# s[i], s[j] = s[j], s[i]
 
-s = s[:i + 1] + sorted(s[i + 1:])
+# s = s[:i + 1] + sorted(s[i + 1:])
 
-print(int(''.join(s))) 
+# print(int(''.join(s))) 
+
+
+# 정리 정돈을 좋아하는 k씨
+n, m = map(int,input().split())
+a = list(map(int,input().split()))
+queries = []
+for _ in range(m) :
+    i,j,k = map(int,input().split())
+    queries.append((i,j,k))
+
+for (i,j,k) in queries :
+    subquery = sorted(a[i-1:j])
+    answer = subquery[k-1]
+    print(answer)

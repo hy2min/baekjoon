edb345d964dee0ed6547fdad24843263cb07a63f
hyemin lee <hy2min@gmail.com>
2024-08-09 14:19:17 +0900

    solve : 스택

--- 스택,큐,덱 단계.py
+++ 스택,큐,덱 단계.py
@@ -47,18 +47,95 @@
 # for answer in answers:
 #     print(answer)
 
-# 10773.제로
-def stack(nums) :
-    stack = []
-    for num in nums :
-        if num == '0' :
-            if stack :
-                stack.pop()
-        else:
-            stack.append(int(num))
-    return sum(stack)
-import sys
-k = int(input())
-nums = [sys.stdin.readline().strip() for _ in range(k)]
-answer = stack(nums)
-print(answer)
+# # 10773.제로
+# def stack(nums) :
+#     stack = []
+#     for num in nums :
+#         if num == '0' :
+#             if stack :
+#                 stack.pop()
+#         else:
+#             stack.append(int(num))
+#     return sum(stack)
+# import sys
+# k = int(input())
+# nums = [sys.stdin.readline().strip() for _ in range(k)]
+# answer = stack(nums)
+# print(answer)
+
+# # 9012.괄호
+# def ps(strings) :
+#     stack = []
+#     for string in strings :
+#         if string == "(" :
+#             stack.append(string)
+#         else :
+#             if not stack :
+#                 return "NO"
+#             stack.pop()
+#     if not stack :
+#         return "YES"
+#     else :
+#         return "NO"
+# t = int(input())
+# for _ in range(t) :
+#     strings = input()
+#     print(ps(strings))
+
+# # 4949.균형잡힌 세상
+# def parentheses(strings) :
+#     stack = []
+#     pair = {
+#         ")" :"(",
+#         "]" : "["
+#     }
+#     for string in strings :
+#         if string in "([" :
+#             stack.append(string)
+#         elif string in ")]" :
+#             if not stack :
+#                 return "no"
+#             top = stack.pop()
+#             if  pair[string] != top :
+#                 return "no"
+#     if not stack :
+#         return "yes"
+#     else :
+#         return "no"
+
+# while True:
+#     strings = input()
+#     if strings == "." :
+#         break
+#     if strings[-1] =='.' :
+#         strings = strings[:-1]
+#     answer = parentheses(strings)
+#     print(answer)
+
+# # 12789.도키도키 간식드리미
+# def snack_sequence(queue) :
+#     stack = []
+#     start_num = 1
+
+#     while queue :
+#         front = queue.pop(0)
+#         if front == start_num :
+#             start_num += 1
+#         else :
+#             stack.append(front)
+        
+#         while stack and stack[-1] == start_num :
+#             stack.pop()
+#             start_num += 1
+        
+#     while stack :
+#         top = stack.pop()
+#         if top != start_num :
+#             return "Sad"
+#         start_num += 1
+#     return "Nice"
+
+# n = int(input())
+# queue = list(map(int,input().split()))
+# print(snack_sequence(queue))
+
